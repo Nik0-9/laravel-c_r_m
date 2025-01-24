3704a8bb61721695c04d14a6469c076f2fd1b42e
Nik0-9 <filannino.nicola9@gmail.com>
2025-01-24 11:42:10 +0100

    feat: AgencyResource with related Model created

---
+++ database/migrations/2025_01_24_102755_create_agencies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('logo_path', 255)->nullable();
+            $table->string('partitaIva', 11)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agencies');
+    }
+};
